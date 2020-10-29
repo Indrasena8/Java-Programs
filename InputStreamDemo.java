caf01a15a728629e3030b2d7b7cf2d9a407716bf
Indrasena Kalyanam <47857838+Indrasena8@users.noreply.github.com>
2020-10-30 04:41:09 +0530

    Add files via upload

---
+++ InputStreamDemo.java
@@ -0,0 +1,30 @@
+import java.io.*;                                                                    
+public class InputStreamDemo                                                             
+{                                                                                    
+    public static void main(String[] args) throws FileNotFoundException, IOException                                                                                     
+    {                                                                               
+                                                                     
+        // initializing FileInputStream  
+        try {
+            FileInputStream geek = new FileInputStream("F:\\Courses\\Programming\\Java\\ABC.txt");       
+            System.out.println("File opened...");                                                                    
+            // Initializing InputStreamReader object                     
+            InputStreamReader in_strm = new InputStreamReader(geek);                                                         
+            int t;                                                      
+            String read_reslt="";                                        
+                                                                        
+            // Use of read() method                                      
+            while((t = in_strm.read()) != -1)                            
+            {                                                            
+                read_reslt = read_reslt+(char)t;                         
+            }                                                            
+                                                                        
+            // print the result read from the file                       
+            System.out.println(read_reslt);                              
+        }     
+        catch(IOException e){
+            System.err.println("File opening failed:");
+            e.printStackTrace();
+        }                     
+    }
+}   
