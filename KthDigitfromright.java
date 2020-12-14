475c0e3e47603dbda4dcfad4dbb7787062d8e5e7
Indrasena Kalyanam <47857838+Indrasena8@users.noreply.github.com>
2020-12-15 00:25:08 +0530

    Add files via upload

---
+++ KthDigitfromright.java
@@ -0,0 +1,37 @@
+import java.util.*; 
+import java.lang.*; 
+  
+public class KthDigitfromright { 
+    // To compute k-th digit in a^b 
+    public static int kthdigit(int a, int b, int k) 
+    { 
+        // Computing a^b 
+        int p = (int)Math.pow(a, b); 
+  
+        int count = 0; 
+        while (p > 0 && count < k) { 
+  
+            // Getting last digit 
+            int rem = p % 10; 
+  
+            // Increasing count by 1 
+            count++; 
+  
+            // If current number is required digit 
+            if (count == k) 
+                return rem; 
+  
+            // Remove last digit 
+            p = p / 10; 
+        } 
+  
+        return 0; 
+    } 
+      
+    // Driver Code  
+    public static void main(String argc[]) { 
+        int a = 5, b = 2; 
+        int k = 1; 
+        System.out.println(kthdigit(a, b, k)); 
+    }    
+} 
